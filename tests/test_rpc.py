cb6fd86862a2c68b4e21cf8e2d479d40db8f98f1
32ethers <somereasons@outlook.com>
2023-05-09 09:46:17 +0800

    debug rpc download

--- tests/test_rpc.py
+++ tests/test_rpc.py
@@ -6,17 +6,21 @@ from demeter_fetch import constants
 from demeter_fetch.source_rpc import query_blockno_from_time, query_event_by_height, ContractConfig, HeightCacheManager
 import demeter_fetch._typing as typing
 from demeter_fetch.eth_rpc_client import EthRpcClient
-from .config import end_point
+import toml
 
 
 class UniLpDataTest(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        self.config = toml.load("./config.toml")
+        super(UniLpDataTest, self).__init__(*args, **kwargs)
+
     # ==========lines=========================
     def test_query_blockno_from_time(self):
         value = query_blockno_from_time(typing.ChainType.ethereum, datetime(2023, 5, 8), True, "127.0.0.1:7890")
         self.assertTrue(value == 17209709, "height not right")
 
     def test_query_event_by_height(self):
-        client = EthRpcClient(end_point, "127.0.0.1:7890")
+        client = EthRpcClient(self.config["end_point"], "127.0.0.1:7890")
         height_cache = HeightCacheManager(typing.ChainType.polygon, "./sample-data")
         files = query_event_by_height(chain=typing.ChainType.polygon,
                                       client=client,
@@ -25,7 +29,7 @@ class UniLpDataTest(unittest.TestCase):
                                       start_height=42447801,
                                       end_height=42448800,
                                       height_cache=height_cache,
-                                      save_path="./sample-data",
+                                      save_path="../sample-data",
                                       save_every_query=2,
                                       batch_size=500)
         height_cache.save()
@@ -33,7 +37,7 @@ class UniLpDataTest(unittest.TestCase):
         self.assertTrue(len(files) == 1)
 
     def test_query_event_by_height_save_rest(self):
-        client = EthRpcClient(end_point, "127.0.0.1:7890")
+        client = EthRpcClient(self.config["end_point"], "127.0.0.1:7890")
         height_cache = HeightCacheManager(typing.ChainType.polygon, "./sample-data")
         files = query_event_by_height(chain=typing.ChainType.polygon,
                                       client=client,
@@ -42,7 +46,7 @@ class UniLpDataTest(unittest.TestCase):
                                       start_height=42447301,  # height difference can not be divided by 2*500
                                       end_height=42448800,
                                       height_cache=height_cache,
-                                      save_path="./sample-data",
+                                      save_path="../sample-data",
                                       save_every_query=2,
                                       batch_size=500)
         height_cache.save()
