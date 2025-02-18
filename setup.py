6ef50594c6ba8570ccd8baf4439f863ef7a21b27
32ethers <somereasons@outlook.com>
2025-02-18 17:15:54 +0800

    fix issue #5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='demeter-fetch',
-    version='1.1.9',
+    version='1.1.10',
     packages=find_packages(exclude=["tests", "tests.*", "samples", "samples.*"]),
     url='',
     license='MIT',
