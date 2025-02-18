9a3646ce5eca17c7e535946a15bdb3ad65285cd5
32ethers <somereasons@outlook.com>
2025-02-18 16:50:15 +0800

    just update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='demeter-fetch',
-    version='1.1.7',
+    version='1.1.8',
     packages=find_packages(exclude=["tests", "tests.*", "samples", "samples.*"]),
     url='',
     license='MIT',
