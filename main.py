cb73a290ef5d63a59947b3ebb39a354adb6cc10c
32ethers <somereasons@outlook.com>
2025-05-09 12:00:36 +0800

    simplify main

--- main.py
+++ main.py
@@ -1,28 +1,4 @@
-import sys
-import demeter_fetch.common.utils as utils
-from demeter_fetch.core import download, get_commend_args
-from demeter_fetch.tools import date_to_height, block_timestamp_cache, aave_risk_param
-
-
-def main():
-    if len(sys.argv) == 1:
-        utils.print_log("use parameter -h for help")
-        exit(1)
-    if len(sys.argv) > 1 and sys.argv[1].endswith(".toml"):
-        download(sys.argv[1])
-        exit(0)
-
-    args = get_commend_args()
-    if args.config is not None:
-        download(args.config)
-    elif args.tools is not None:
-        if args.tools == "date_to_height":
-            date_to_height(args)
-        elif args.tools == "block_timestamp":
-            block_timestamp_cache(args)
-        elif args.tools == "aave":
-            aave_risk_param(args)
-        pass
+import demeter_fetch.main
 
 if __name__ == "__main__":
-    main()
+    demeter_fetch.main.main()
